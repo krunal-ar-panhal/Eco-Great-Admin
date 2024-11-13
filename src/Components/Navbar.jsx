45d89819dd5a58a3af0a235e90aef9f863a7a722
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-11-13 15:56:07 +0530

    navabr ko sticky kiya

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
   return (
-    <div className="w-full bg-white  text-gray-200 px-8 py-4 flex justify-between items-center shadow-md">
+    <div className="w-full bg-white text-gray-200 px-8 py-4 flex justify-between items-center shadow-md sticky top-0 z-50">
       <img src="./logo.png" alt="logo" className="w-32" />
-      <button onClick={()=>setToken('')} className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-200">
+      <button
+        onClick={() => setToken('')}
+        className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-200"
+      >
         Logout
       </button>
     </div>
